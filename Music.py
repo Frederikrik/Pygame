9109a7b8b92a6cbd5ec0a5c802f941b1500831b1
Frederik <frederik.guentensperger@uzh.ch>
2024-11-14 15:19:36 +0100

    hi

--- Music.py
+++ Music.py
@@ -2,7 +2,7 @@
 pygame.init()
 pygame.mixer.init()
 # Load and play background music
-pygame.mixer.music.load("sounds/background_music.mp3")  # Path to your music file
+pygame.mixer.music.load("background_music.mp3")  # Path to your music file
 pygame.mixer.music.set_volume(0.5)  # Set volume level (0.0 to 1.0)
 pygame.mixer.music.play(-1)  # -1 means the music will loop indefinitely
 
